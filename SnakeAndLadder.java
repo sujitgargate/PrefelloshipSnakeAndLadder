a13cae866e0d72bd2e1186e3d62f7950bb6b96af
Sujit gargate <sujitgargate5893@gmail.com>
2020-08-02 18:20:46 +0530

    Refactored : Switch Case ,Code as per suggestions

---
+++ SnakeAndLadder.java
@@ -0,0 +1,59 @@
+import java.util.Random;
+
+class SnakeAndLadder{
+
+   //variable declaration and Constants
+   int currentPosition = 0;
+   int diceResult = 0;
+   int WINNINGPOSITION = 100;
+
+   public static void main(String args[]){
+
+      SnakeAndLadder obj = new SnakeAndLadder();
+      System.out.println("Player is at Position : " + obj.checkOptions());
+   }
+
+   //dieRoll() method will act as Thrown physical dice
+
+   public int dieRoll(int rollLimit){
+      Random rand = new Random();
+      int diceRoll = (rand.nextInt(rollLimit)) + 1;
+      return diceRoll;
+   }
+
+   public int checkOptions(){
+
+      //Here Player wont stop untill it's current Position is 100
+
+      while(currentPosition < WINNINGPOSITION){
+         int checkOptionsForPlay = dieRoll(3);
+
+         switch(checkOptionsForPlay){
+            case 1:
+               System.out.println("\nNo play and Current Position is " + currentPosition);
+            break;
+
+            case 2:
+               diceResult = dieRoll(6);
+
+               //Check if Player's position is less or equal to 0, then position=0
+               if(currentPosition <= diceResult){
+                  currentPosition = 0;
+                  System.out.println("\nYou got Snake with dice face of " + diceResult + " and Current Position is " + currentPosition);
+               }
+               else{
+                  currentPosition -= diceResult;
+                  System.out.println("\nYou got Snake with dice face of " + diceResult + " and Current Position is " + currentPosition);
+               }
+            break;
+
+            case 3:
+               diceResult = dieRoll(6);
+               currentPosition += diceResult;
+               System.out.println("\nYou got Ladder with dice face of " + diceResult + " and Current Position is " + currentPosition);
+            break;
+         }
+      }
+      return currentPosition;
+   }
+}
