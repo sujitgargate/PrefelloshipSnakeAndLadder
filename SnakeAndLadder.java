27715bda6fcc158739386a5122333d1623e9af9e
sujit gargate <sujitgargate5893@gmail.com>
2020-08-02 20:44:46 +0530

    Added : Player will get Exact Winning Position

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -1,61 +1,70 @@
 import java.util.Random;
 
-class UC4{
+class SnakeAndLadder{
 
-   //variable declaration and Constants
-   int currentPosition = 0;
-   int diceResult = 0;
-   int WINNINGPOSITION = 100;
+    //variable declaration and Constants
+    int currentPosition = 0;
+    int diceResult = 0;
+    int WINNINGPOSITION = 100;
 
-   public static void main(String args[]){
+    public static void main(String args[]){
 
-      UC4 obj = new UC4();
-      System.out.println("Player is at Position : " + obj.checkOptions());
-   }
+        SnakeAndLadder obj = new SnakeAndLadder();
+        System.out.println("Player is at Position : " + obj.checkOptions());
+    }
 
-   //dieRoll() method will act as Thrown physical dice
+    //dieRoll() method will act as Thrown physical dice
+    public int dieRoll(int rollLimit){
+        Random rand = new Random();
+        int diceRoll = (rand.nextInt(rollLimit)) + 1;
+        return diceRoll;
+    }
 
-   public int dieRoll(int rollLimit){
-      Random rand = new Random();
-      int diceRoll = (rand.nextInt(rollLimit)) + 1;
-      return diceRoll;
-   }
+    public int checkOptions(){
 
-   public int checkOptions(){
+        //Here Player wont stop untill it's current Position is 100
+        while(currentPosition < WINNINGPOSITION ){
+            int checkOptionsForPlay = dieRoll(3);
 
-      //Here Player wont stop untill it's current Position is 100
+            switch(checkOptionsForPlay){
+                case 1:
+                    System.out.println("\nNo play and Current Position is " + currentPosition);
+                    break;
 
-      while(currentPosition < WINNINGPOSITION){
-         int checkOptionsForPlay = dieRoll(3);
+                case 2:
+                    diceResult = dieRoll(6);
 
-         switch(checkOptionsForPlay){
-            case 1:
-               System.out.println("\nNo play and Current Position is " + currentPosition);
-            break;
+                    //Check if Player's position is less or equal to 0, then position=0
+                    if(currentPosition <= diceResult){
+                        currentPosition = 0;
+                        System.out.println("\nYou got Snake with dice face of " + diceResult + " and Current Position is " + currentPosition);
+                    }
+                    else{
+                        currentPosition -= diceResult;
+                        System.out.println("\nYou got Snake with dice face of " + diceResult + " and Current Position is " + currentPosition);
+                    }
+                    break;
 
-            case 2:
-               diceResult = dieRoll(6);
+                case 3:
+                    diceResult = dieRoll(6);
 
-               //Check if Player's position is less or equal to 0, then position=0
-               if(currentPosition <= diceResult){
-                  currentPosition = 0;
-                  System.out.println("\nYou got Snake with dice face of " + diceResult + " and Current Position is " + currentPosition);
-               }
-               else{
-                  currentPosition -= diceResult;
-                  System.out.println("\nYou got Snake with dice face of " + diceResult + " and Current Position is " + currentPosition);
-               }
-            break;
+                    //current Position + dice should not exceed winning position
+                    if(currentPosition + diceResult > WINNINGPOSITION){
 
-            case 3:
-               diceResult = dieRoll(6);
-               currentPosition += diceResult;
-               System.out.println("\nYou got Ladder with dice face of " + diceResult + " and Current Position is " + currentPosition);
-            break;
-         }
-      }
-      return currentPosition;
-   }
+                        //Dice roll Should result into exact number to Winning Position
+                        while(currentPosition + diceResult == WINNINGPOSITION){
+                            diceResult = dieRoll(6);
+                            currentPosition += diceResult;
+                        }
+                        break;
+                    }
+                    else {
+                        currentPosition += diceResult;
+                        System.out.println("\nYou got Ladder with dice face of " + diceResult + " and Current Position is " + currentPosition);
+                        break;
+                    }
+            }
+        }
+        return currentPosition;
+    }
 }
-
-
